f1175f07b74626e9d8078ee443b471c2ed6cfc2d
JeffersonBarefoot <Jefferson.L.Barefoot@gmail.com>
2025-02-15 22:05:25 -0500

    + push controller variables to blade

--- resources/views/positions.blade.php
+++ resources/views/positions.blade.php
@@ -2,13 +2,20 @@
 
 echo "Positions:  Hello World!";
 
+
+
+
 ?>
 
 {{--@dump("Blade ID:")--}}
 {{--@dump($id)--}}
 <br><br><br><br>
+{{$id}}<br>
+{{$test}}<br>
+{{$test2}}<br>
+{{$test3}}<br>
 
-
+<br><br><br><br>
 <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
 {{ __('Back to the Dashboard') }}
 </x-nav-link>
