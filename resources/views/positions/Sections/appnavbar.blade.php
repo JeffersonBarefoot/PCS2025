25e82cb05ea0ebaba558ec4921dca58d7ac4a784
JeffersonBarefoot <Jefferson.L.Barefoot@gmail.com>
2025-02-18 07:51:37 -0500

    Intermed backup, divs and bootstrap

---
+++ resources/views/positions/Sections/appnavbar.blade.php
@@ -0,0 +1,18 @@
+<body>
+
+<div class="anbdiv">
+    TopNavigation<br><br>
+
+    Hello World, this is Position<br><br>
+    <br><br>
+    {{"Positon ID:  ".$id}}<br>
+    {{"Position Description:  ".$position->descr}}<br><br>
+
+    <br><br><br><br>
+    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+        {{ __('Back to the Dashboard') }}
+    </x-nav-link>
+    <br><br>
+</div>
+
+</body>
--- resources/views/positions/show.blade.php
+++ resources/views/positions/show.blade.php
@@ -5,73 +5,26 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <style>
-
+        .anbdiv {
+            border: 5px outset red;
+            background-color: lightblue;
+            text-align: center;
+        }
     </style>
     <title></title>
 
 </head>
 
-Hello World, this is Position<br><br>
-<br><br>
-{{"Positon ID:  ".$id}}<br>
-{{"Position Description:  ".$position->descr}}<br><br>
 
-{{--{{$test}}<br>--}}
-{{--{{$test2}}<br>--}}
-{{--{{$test3}}<br>--}}
-
-<br><br><br><br>
-<x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-{{ __('Back to the Dashboard') }}
-</x-nav-link><br><br>
-
-{{--<home :user="user" inline-template>--}}
-{{--<home inline-template>--}}
-{{--    <div class="container-fluid">--}}
-{{--        <!-- Application Dashboard -->--}}
-{{--        <!-- <div class="row justify-content-center"> -->--}}
-{{--        <div class="row">--}}
-{{--            <!-- <div class="col-md-8"> -->--}}
-{{--            <div class="col">--}}
-{{--                <!-- <div class="card card-default"> -->--}}
-This is where the nav bar goes when we get it working.<br>
-                <!-- test verbiage -->
+@include('positions.sections.appnavbar')
 @include('positions.sections.datanavbar')
 
-{{--                <!-- </div> -->--}}
 
-{{--            </div>--}}
 
-{{--        </div>--}}
 
-{{--    </div>--}}
 
-{{--</home>--}}
 
-<!-- ************************** -->
-<!-- ************************** -->
-<!-- bootstrap sample -->
-<!-- ************************** -->
-<!-- ************************** -->
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h4 class="panel-title">
-            <div class="row">
-                <div class="col-md-2">
-                    <a data-toggle="collapse" href="#collapse6">Sample Title</a>
-                </div>
-                <div class="col-md-10">
-                </div>
-            </div>
-        </h4>
-    </div>
-    <div id="collapse6" class="panel-collapse collapse">
-        <div class="panel-body">xxxx Bootstrap collapsable div xxxx
 
-        </div>
-    </div>
-</div>
 
-{{--{{round($position->fulltimeequiv,3)}} FTEs / {{$position->budgsal}}--}}
 
 
