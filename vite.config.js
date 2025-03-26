a8be6dfc1de0c1408bdf476247d9bd3c3833d5ad
JeffersonBarefoot <Jefferson.L.Barefoot@gmail.com>
2025-03-26 09:21:44 -0400

    - After a long period of everything being broken, finally got Vite working again.  Had to uninstall and reinstall vite, see notes in ErrorKnowledgebase.xlsx - See "Hello Vite!" box in 001Head.blade.php - Added some graphic elements from prior version - arrows mostly - Collapsible cards are NOT working...they open and then immediately collapse again

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         outDir: 'public/build',
     },
     server: { // from https://github.com/laravel/framework/discussions/46928 JLB 20250320
+        // host: '127.0.0.1',  // Add this to force IPv4 only JLB 20250325 https://stackoverflow.com/questions/73783480/neterr-connection-refused-using-laravel-9-reactjs-with-vite-js/78344285#78344285
         port: 5173,
         watch: {
             ignored: [
